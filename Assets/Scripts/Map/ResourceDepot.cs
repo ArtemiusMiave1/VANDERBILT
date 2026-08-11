d42e731a6e1635de80b226045b3af87506ab4a3d
ArtemiusMiave1 <artemiusmiave@y7mail.com>
2026-08-11 19:34:00 +1000

    scaling down

---
+++ Assets/Scripts/Map/ResourceDepot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ResourceDepot : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
