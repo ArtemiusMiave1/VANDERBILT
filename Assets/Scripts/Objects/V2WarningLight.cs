c182cce0480fbefa4b106710a9270f524144e730
StickyStupid <stickyisstupid456@gmail.com>
2026-08-20 16:59:16 +1000

    Alarm V2 Fix
    type "Red" for red light, "Purple" for purple light and "Neither" for off

--- Assets/Scripts/Objects/V2WarningLight.cs
+++ Assets/Scripts/Objects/V2WarningLight.cs
@@ -4,37 +4,59 @@ using UnityEngine;
 public class V2WarningLight : MonoBehaviour
 {
 
-    public bool RedLightToggle;
-    public bool PurpleLightToggle;
+    //public bool RedLightToggle;
+    //public bool PurpleLightToggle;
+    public string CurrentColor; // change color here
     public Material materialVar;
+    public Animator animatorVar;
+    int RedHash;
+    int PurpleHash;
+    int OffHash;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Animator animatorVar = GetComponent<Animator>();
+        PurpleHash = Animator.StringToHash("Base Layer.PurpleLight");
+        RedHash = Animator.StringToHash("Base Layer.RedLight");
+        OffHash = Animator.StringToHash("Base Layer.BothOff");
+
 
     }
 
     // Update is called once per frame
     void Update()
-    {   // debug toggles in editor
-        if (RedLightToggle)
-        {
-            Light("Red");
-            LightOff("Purple");
-            PurpleLightToggle = false;
-        }
 
-        if (PurpleLightToggle)
+    {   switch (CurrentColor)
         {
-            Light("Purple");
-            LightOff("Red");
-            RedLightToggle = false;
-        }
+            case "Purple":
+                Light("Purple");
+                break;
+            case "Red":
+                Light("Red");
+                break;
+            case "Neither":
+                LightOff("Both");
+                break;
 
-        if (!RedLightToggle && !PurpleLightToggle)
-        {
-            LightOff("Both");
         }
+        
+        // debug toggles in editor
+        //if (RedOurPurple)
+        //{
+           // Light("Red");
+           // PurpleLightToggle = false;
+        //}
+
+        //if (PurpleLightToggle)
+        //{
+            //Light("Purple");
+            //RedLightToggle = false;
+       // }
+
+        //else
+        //{
+        //    LightOff("Both");
+        //}
     }
 
     void Light(string colorType)
@@ -43,9 +65,13 @@ public class V2WarningLight : MonoBehaviour
         {
             case "Purple":
                 print("lol");
+                LightOff("Red");
+                animatorVar.Play(PurpleHash);
                 break;
             case "Red":
                 print("aaa");
+                LightOff("Purple");
+                animatorVar.Play(RedHash);
                 break;
         }
 
@@ -63,6 +89,7 @@ public class V2WarningLight : MonoBehaviour
                 break;
             case "Both":
                 print("both offff");
+                animatorVar.Play(OffHash);
                 break;
 
 
