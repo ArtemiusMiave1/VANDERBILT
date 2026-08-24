78d284ff9f3f37cafae4198379a0df8d8a2f34aa
StickyStupid <stickyisstupid456@gmail.com>
2026-08-24 11:59:59 +1000

    vent minigame wip push
    wip of the vent minigame i didnt get to finish cause my power went out

--- Assets/Scripts/Objects/VentMinIGame.cs
+++ Assets/Scripts/Objects/VentMinIGame.cs
@@ -7,25 +7,68 @@ public class VentMinIGame : MonoBehaviour
     public GameObject Vent;
     public GameObject ValveTurn;
     float rotationSpeed = 5f;
+    [SerializeField] private float cur_HP;
+    private const float max_HP = 100;
+    private Vector3 PrevmousePos;
+    //private bool SpaceIsPressed;
+    // Start is called before the first frame update
     void Start()
     {
-        
+        cur_HP = max_HP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ValveTurn.transform.eulerAngles.z >= 30f && ValveTurn.transform.eulerAngles.z > -30f)
-        {
-            print("doneeee");
-        }
+        //SpaceIsPressed = Input.GetKey(KeyCode.Space);
+        Wheel();
+
+
     }
 
-    void OnMouseDrag()
+    void Wheel()
     {
-        float zRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-        ValveTurn.transform.Rotate(Vector3.forward, zRotation);
-        print("spinninggggg");
+        Vector3 mouseDelta = Input.mousePosition - PrevmousePos;
 
+        if (mouseDelta.x > 0)
+        {
+            float amount = 0.01f;
+            cur_HP -= mouseDelta.x * amount;
+        }
+        else if (mouseDelta.x < 0)
+        {
+            float amount = 0.01f;
+            cur_HP += mouseDelta.x * amount;    
+        }
+        if (cur_HP <= 0)
+        {
+            cur_HP = 0;
+            Debug.Log("You have unlocked");
+        }
+        if (cur_HP > 100)
+        {
+            cur_HP = 100;
+        }
+        ValveTurn.transform.localRotation = Quaternion.Euler(0, 0, (720 / max_HP * cur_HP));
+        PrevmousePos = Input.mousePosition;
     }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (ValveTurn.transform.eulerAngles.z >= 30f && ValveTurn.transform.eulerAngles.z > -30f)
+    //    {
+    //        print("doneeee");
+    //    }
+    //}
+
+    //void OnMouseDrag()
+    //{
+    //    float zRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+    //    ValveTurn.transform.Rotate(Vector3.forward, zRotation);
+    //   // float xRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+    //    //ValveTurn.transform.transform.Rotate(Vector3.forward, xRotation);
+    //    print("spinninggggg");
+
+    //}
 }
