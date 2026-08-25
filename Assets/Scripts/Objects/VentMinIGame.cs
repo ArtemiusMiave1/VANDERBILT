4cba3dfadc59ca524de583f5538d8e9cf12e7d05
StickyStupid <stickyisstupid456@gmail.com>
2026-08-25 19:04:25 +1000

    Vent Minigame WIP
    functional, but has two bugs listed in the code comments

--- Assets/Scripts/Objects/VentMinIGame.cs
+++ Assets/Scripts/Objects/VentMinIGame.cs
@@ -2,73 +2,112 @@ using UnityEngine;
 
 public class VentMinIGame : MonoBehaviour
 {
+    // credit to the script https://stackoverflow.com/questions/56152299/realistic-valve-wheel-rotation-in-unity
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public GameObject Vent;
     public GameObject ValveTurn;
-    float rotationSpeed = 5f;
+    Animator ventAnimator;
+
+    public float rotationSpeed = 0.05f;
     [SerializeField] private float cur_HP;
+
     private const float max_HP = 100;
     private Vector3 PrevmousePos;
-    //private bool SpaceIsPressed;
+    private bool ClickedOn;
+
     // Start is called before the first frame update
     void Start()
     {
         cur_HP = max_HP;
+       ventAnimator = Vent.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //SpaceIsPressed = Input.GetKey(KeyCode.Space);
+        if (Input.GetMouseButton(0)) // to fix: make it so it only moves when the mouse is ON THE OBJECT !!!! players can click on the valve and move their mouse the other way
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform != null)
+                {
+                    print(hit); // this doesnt really matter cause the handle is the only one with collision lol
+                    if (hit.transform.name == "pipe_handle")
+                    {
+                        ClickedOn = true;
+                    }
+                    else
+                    {
+                        ClickedOn = false;
+                    }
+                }
+            }
+        }
+        if (!Input.GetMouseButton(0))
+        {
+            ClickedOn = false;
+        }
         Wheel();
-
-
     }
 
-    void Wheel()
-    {
-        Vector3 mouseDelta = Input.mousePosition - PrevmousePos;
+        void Wheel()
+        {
+            Vector3 mouseDelta = Input.mousePosition - PrevmousePos;
 
-        if (mouseDelta.x > 0)
-        {
-            float amount = 0.01f;
-            cur_HP -= mouseDelta.x * amount;
+            if (mouseDelta.x > 0 && ClickedOn)
+            {
+                cur_HP -= mouseDelta.x * rotationSpeed;
+            }
+            else if (mouseDelta.x < 0 && ClickedOn)
+            {
+                cur_HP += mouseDelta.x * rotationSpeed;
+            }
+            if (cur_HP <= 0)
+            {
+                cur_HP = 0;
+                print("open vent");
+                OpenVent();
+            }
+            if (cur_HP > 100)
+            {
+                cur_HP = 100;
+            }
+            ValveTurn.transform.localRotation = Quaternion.Euler(0, 0, (720 / max_HP * cur_HP)); // to fix: move only 80 degrees
+            PrevmousePos = Input.mousePosition;
         }
-        else if (mouseDelta.x < 0)
-        {
-            float amount = 0.01f;
-            cur_HP += mouseDelta.x * amount;    
-        }
-        if (cur_HP <= 0)
-        {
-            cur_HP = 0;
-            Debug.Log("You have unlocked");
-        }
-        if (cur_HP > 100)
+
+        void ResetWheel()
         {
             cur_HP = 100;
+            ValveTurn.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            ventAnimator.SetBool("VentOpenBool", false);
+        
         }
-        ValveTurn.transform.localRotation = Quaternion.Euler(0, 0, (720 / max_HP * cur_HP));
-        PrevmousePos = Input.mousePosition;
-    }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (ValveTurn.transform.eulerAngles.z >= 30f && ValveTurn.transform.eulerAngles.z > -30f)
-    //    {
-    //        print("doneeee");
-    //    }
-    //}
+        void OpenVent()
+        {
+            ventAnimator.SetBool("VentOpenBool", true);
+        }
 
-    //void OnMouseDrag()
-    //{
-    //    float zRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-    //    ValveTurn.transform.Rotate(Vector3.forward, zRotation);
-    //   // float xRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
-    //    //ValveTurn.transform.transform.Rotate(Vector3.forward, xRotation);
-    //    print("spinninggggg");
+        // Update is called once per frame
+        //void Update()
+        //{
+        //    if (ValveTurn.transform.eulerAngles.z >= 30f && ValveTurn.transform.eulerAngles.z > -30f)
+        //    {
+        //        print("doneeee");
+        //    }
+        //}
 
-    //}
+        //void OnMouseDrag()
+        //{
+        //    float zRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        //    ValveTurn.transform.Rotate(Vector3.forward, zRotation);
+        //   // float xRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+        //    //ValveTurn.transform.transform.Rotate(Vector3.forward, xRotation);
+        //    print("spinninggggg");
+
+        //}
 }
