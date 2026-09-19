804dcb1a6e73da5e7d04069737e4f84bc71a233e
thousandsunny05 <so.sunnywaiyin@gmail.com>
2026-09-19 17:00:36 +1000

    Fuel Tank Texture and Gloosary Start

--- Assets/Scripts/Book/BookInteract.cs
+++ Assets/Scripts/Book/BookInteract.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class BookInteraction : MonoBehaviour
 {
     public Book book;
-
     public void OpenBook()
     {
         if (book != null)
         {
             book.OpenBook();
         }
+
     }
 }
